483ae61b740d8fca2fcd68dd1565ec05c48774d0
James Cox <th3james@gmail.com>
2013-07-23 12:01:23 +0100

    Show title in report view also

--- clientTests/report_page.coffee
+++ clientTests/report_page.coffee
@@ -1,19 +1,41 @@
 assert = chai.assert
 
+factoryReport = (attributes) ->
+  report = new Backbone.Models.Report
+  report.set(attributes)
+  return report
+
+createAndShowReportViewForReport = (report) ->
+  view = new Backbone.Views.ReportView(report: report)
+  $('#test-container').html(view.el)
+  return view
+
 suite('Report View')
 
+test("Can see a report's title", ->
+  title = "My Lovely Report"
+  report = factoryReport(title: title)
+
+  view = createAndShowReportViewForReport(report)
+  
+  assert.match(
+    $('#test-container').text(),
+    new RegExp(".*#{title}.*")
+  )
+
+  view.close()
+)
+
 test("Can see a report's overview", ->
-  report = new Backbone.Models.Report
   overviewText = "Hey, I'm an overview"
-  report.set('overview', overviewText)
+  report = factoryReport(overview: overviewText)
 
-  view = new Backbone.Views.ReportView(report: report)
-
-  $('#test-container').html(view.el)
+  view = createAndShowReportViewForReport(report)
   
   assert.match(
     $('#test-container').text(),
     new RegExp(".*#{overviewText}.*")
   )
+
   view.close()
 )
--- public/test/js/tests.js
+++ public/test/js/tests.js
@@ -1,19 +1,44 @@
 (function() {
-  var assert;
+  var assert, createAndShowReportViewForReport, factoryReport;
 
   assert = chai.assert;
 
-  suite('Report View');
-
-  test("Can see a report's overview", function() {
-    var overviewText, report, view;
+  factoryReport = function(attributes) {
+    var report;
     report = new Backbone.Models.Report;
-    overviewText = "Hey, I'm an overview";
-    report.set('overview', overviewText);
+    report.set(attributes);
+    return report;
+  };
+
+  createAndShowReportViewForReport = function(report) {
+    var view;
     view = new Backbone.Views.ReportView({
       report: report
     });
     $('#test-container').html(view.el);
+    return view;
+  };
+
+  suite('Report View');
+
+  test("Can see a report's title", function() {
+    var report, title, view;
+    title = "My Lovely Report";
+    report = factoryReport({
+      title: title
+    });
+    view = createAndShowReportViewForReport(report);
+    assert.match($('#test-container').text(), new RegExp(".*" + title + ".*"));
+    return view.close();
+  });
+
+  test("Can see a report's overview", function() {
+    var overviewText, report, view;
+    overviewText = "Hey, I'm an overview";
+    report = factoryReport({
+      overview: overviewText
+    });
+    view = createAndShowReportViewForReport(report);
     assert.match($('#test-container').text(), new RegExp(".*" + overviewText + ".*"));
     return view.close();
   });
