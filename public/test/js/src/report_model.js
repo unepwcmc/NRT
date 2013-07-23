c11c210839db3415b95ca38e29cb4f1fb62e22f4
James Cox <th3james@gmail.com>
2013-07-23 14:32:17 +0100

    Move bower libs, add backbone associations

---
+++ public/test/js/src/report_model.js
@@ -0,0 +1,12 @@
+(function() {
+  var assert;
+
+  assert = chai.assert;
+
+  suite('Backbone.Model.Report');
+
+  test('Can add sections', function() {
+    return console.log("TODO");
+  });
+
+}).call(this);
